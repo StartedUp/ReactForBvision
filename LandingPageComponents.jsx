024945097acfca690154ea15ad875095da57756c
PrithuGit <prithviprakashr@gmail.com>
2016-05-19 13:14:04 +0530

    New claim Page added

--- LandingPageComponents.jsx
+++ LandingPageComponents.jsx
@@ -9,11 +9,19 @@
  var RadioButton = React.createClass({
  	render: function(){
  		return (
- 			
 			    <label className="radio-inline">
-			      <input type="radio" name="optradio" />{this.props.radioName}
+			      <input type="radio" name="optradio" data-toggle="modal" data-target={this.props.Toggle}/>{this.props.radioName}
 			    </label>
-  			
+ 		);
+ 	}
+ });
+ var TextBox = React.createClass({
+ 		render: function(){
+ 		return (
+ 			<div className="form-group">
+      <label for={this.props.Label}>{this.props.Label}</label>
+      <input type="text" className="form-control" id={this.props.Label} placeholder={this.props.PlaceValue} />
+    </div>
  		);
  	}
  });
